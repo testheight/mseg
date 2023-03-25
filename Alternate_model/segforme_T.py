517bf90457dd487852612be9668cee85f9054052
testheight <318901249@qq.com>
2023-03-25 21:40:49 +0800

    x

--- Alternate_model/segforme_T.py
+++ Alternate_model/segforme_T.py
@@ -288,12 +288,37 @@ class Segformer_primary(nn.Module):
             nn.Conv2d(dim, decoder_dim, 1),
             nn.Upsample(scale_factor = 2 ** i)
         ) for i, dim in enumerate(dims)])
+
+        # 原始分类方法      &&-----已训练-----&&
+        # self.to_segmentation = nn.Sequential(
+        #     nn.Conv2d(4 * decoder_dim, decoder_dim, 1),
+        #     nn.Conv2d(decoder_dim, num_classes, 1),
+        #     ##(2,2,128,128)
+        #     nn.Upsample(scale_factor=4, mode='bilinear', align_corners=False))
         
         # 改进最后的解码头   
         self.to_segmentation = nn.Sequential(
             nn.Upsample(scale_factor=4, mode='bilinear', align_corners=False),
             SingleConv(4 * decoder_dim, decoder_dim, 1),
             nn.Conv2d(decoder_dim, num_classes, 1))
+        
+    #     #初始化函数方法
+    #     self.apply(self._init_weights)
+
+    # def _init_weights(self, m):
+    #     if isinstance(m, nn.Linear):
+    #         trunc_normal_(m.weight, std=.02)
+    #         if isinstance(m, nn.Linear) and m.bias is not None:
+    #             nn.init.constant_(m.bias, 0)
+    #     elif isinstance(m, nn.LayerNorm):
+    #         nn.init.constant_(m.bias, 0)
+    #         nn.init.constant_(m.weight, 1.0)
+    #     elif isinstance(m, nn.Conv2d):
+    #         fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+    #         fan_out //= m.groups
+    #         m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+    #         if m.bias is not None:
+    #             m.bias.data.zero_()
 
     def forward(self, x):           
         b,n,h,w = x.shape
@@ -306,6 +331,60 @@ class Segformer_primary(nn.Module):
         fused = torch.cat(fused, dim = 1)
         (2,1024,128,128)
         output = self.to_segmentation(fused)
+        (2,2,512,512)
+        return output
+
+class Segformer_upsample(nn.Module):
+    '''
+    改变上采样方法（亚像素和反卷积）
+    '''
+    def __init__(
+        self, *, dims = (32, 64, 160, 256), heads = (1, 2, 5, 8), ff_expansion = (8, 8, 4, 4),
+        reduction_ratio = (8, 4, 2, 1), num_layers = 2, channels = 3, decoder_dim = 256, num_classes = 2
+    ):
+        super().__init__()
+        dims, heads, ff_expansion, reduction_ratio, num_layers = map(partial(cast_tuple, depth = 4), (dims, heads, ff_expansion, reduction_ratio, num_layers))
+        assert all([*map(lambda t: len(t) == 4, (dims, heads, ff_expansion, reduction_ratio, num_layers))]), 'only four stages are allowed, all keyword arguments must be either a single value or a tuple of 4 values'
+
+        self.mit = MiT(
+            channels = channels,
+            dims = dims,
+            heads = heads,
+            ff_expansion = ff_expansion,
+            reduction_ratio = reduction_ratio,
+            num_layers = num_layers)
+        
+        # 原始特征融合方法
+        self.to_fused = nn.ModuleList([nn.Sequential(
+            nn.Conv2d(dim, decoder_dim, 1),
+            nn.Upsample(scale_factor = 2 ** i)
+        ) for i, dim in enumerate(dims)])
+
+        # 改方法1(亚像素)   &&-----已训练-----&&
+        self.to_segmentation1 = nn.Sequential(
+            nn.PixelShuffle(4),
+            nn.Conv2d(decoder_dim//4, num_classes, 1),)
+        
+        # 改方法2(反卷积)   &&-----已训练-----&&
+        self.to_segmentation2 = nn.Sequential(
+            nn.Conv2d(4 * decoder_dim, decoder_dim, 1),
+            #(1,256,128,128)
+            nn.ConvTranspose2d(decoder_dim,decoder_dim//2, kernel_size=2, stride=2),
+            #(1,128,256,256)
+            nn.ConvTranspose2d(decoder_dim//2,num_classes, kernel_size=2, stride=2),)
+            #(1,2,512,512)
+
+    def forward(self, x):           
+        b,n,h,w = x.shape
+        #(2,3,512,512)
+        layer_outputs = self.mit(x, return_layer_outputs = True)
+        #(2,32,128,128),(2,64,64,64),(2,160,32,32),(2,256,16,16)
+
+        # 原始的方法
+        fused = [to_fused(output) for output, to_fused in zip(layer_outputs, self.to_fused)]
+        fused = torch.cat(fused, dim = 1)
+        (2,1024,128,128)
+        output = self.to_segmentation2(fused)
         (2,2,512,512)
         return output
 
@@ -329,7 +408,7 @@ class Segformer_unet(nn.Module):
             reduction_ratio = reduction_ratio,
             num_layers = num_layers)
         
-        #改进类似unet解码结构             &&-----method1-----&&
+        #改进类似unet解码结构 method1            &&-----已训练-----&&
         self.dc1 = DoubleConv(dims[-1],dims[-1])
         self.up1 = up_conv(dims[-1],dims[-2])
         self.dc2 = DoubleConv(dims[-2]*2,dims[-2])
@@ -342,7 +421,7 @@ class Segformer_unet(nn.Module):
             SingleConv(dims[0], dims[0], 1),
             nn.Conv2d(dims[0], num_classes, 1),)
 
-        # ## 改进类似unet解码结构（增加低维卷积特征提取）     &&-----method2-----&&
+        # ## 改进类似unet解码结构（增加低维卷积特征提取）  method2     &&-----已训练-----&&
         # self.low_conv = SingleConv(3, dims[0], kernel_size=7, stride=2, padding=3)
         # self.dc1 = DoubleConv(dims[-1],dims[-1])
         # self.up1 = up_conv(dims[-1],dims[-2])
@@ -359,13 +438,32 @@ class Segformer_unet(nn.Module):
         #     SingleConv(dims[0]*2,dims[0]*2,kernel_size=1),
         #     nn.Conv2d(dims[0]*2, num_classes, 1))
 
+        # # 通道注意力机制加亚像素           
+        # self.abam0 = cbam_block(dims[0])
+        # self.abam1 = cbam_block(dims[1])
+        # self.abam2 = cbam_block(dims[2])
+        # self.abam3 = cbam_block(dims[3])
+
+        # self.dc1 = DoubleConv(dims[3],dims[3])
+        # self.up1 = nn.PixelShuffle(2)
+        # self.dc2 = DoubleConv(dims[3]//4+dims[2],dims[2])
+        # self.up2 = nn.PixelShuffle(2)
+        # self.dc3 = DoubleConv(dims[2]//4+dims[1],dims[1])
+        # self.up3 = nn.PixelShuffle(2)
+        # self.dc4 = DoubleConv(dims[1]//4+dims[0],dims[0])
+        # self.to_segmentation = nn.Sequential(
+        #     nn.Upsample(scale_factor=4, mode='bilinear', align_corners=False),
+        #     SingleConv(4 * decoder_dim, decoder_dim, 1),
+        #     nn.Conv2d(dims[0], num_classes, 1),)
+
+
     def forward(self, x):           
         b,n,h,w = x.shape
         #(2,3,512,512)
         layer_outputs = self.mit(x, return_layer_outputs = True)
         #(2,32,128,128),(2,64,64,64),(2,160,32,32),(2,256,16,16)
 
-        # ##&&-----method1-----&&##
+        # methond1
         x_c = self.dc1(layer_outputs[3])
         x_c = self.up1(x_c)
         x_c = self.dc2(torch.cat([x_c,layer_outputs[2]], dim=1))
@@ -376,7 +474,7 @@ class Segformer_unet(nn.Module):
         # (2,32,128,128)
         output = self.to_segmentation(x_c)
 
-        # ##&&-----method2-----&&##
+        # ## methond2 增加低维卷积特征提取
         # x_l= self.low_conv(x)
         # # (1,32,256,256)
         # x_c = self.dc1(layer_outputs[-1])
@@ -389,8 +487,81 @@ class Segformer_unet(nn.Module):
         # # (2,32,128,128)
         # output = self.to_segmentation(torch.cat([F.interpolate(x_c,scale_factor=2),x_l],dim=1))
 
+        # ## methond3 师兄之改
+        # x_c = self.dc1(self.abam3(layer_outputs[3]))
+        # # (2,256,16,16)
+        # x_c = self.up1(x_c)
+        # # (2,64,32,32)
+        # x_c = torch.cat([x_c,self.abam2(layer_outputs[2])], dim=1)
+
+        # x_c = self.dc2(x_c)
+        # x_c = self.up2(x_c)
+        # x_c = self.dc3(torch.cat([x_c,self.abam1(layer_outputs[1])], dim=1))
+        # x_c = self.up3(x_c)
+        # x_c = self.dc4(torch.cat([x_c,self.abam0(layer_outputs[0])], dim=1))
+        # # (2,32,128,128)
+        # output = self.to_segmentation(x_c)
+
         return output
 
+class Segformer_deeplabv3plus(nn.Module):
+    '''
+    采用类似deeplabv3plus的解码器结构  +  有无低维特征提取
+    '''
+    def __init__(
+        self, *, dims = (32, 64, 160, 256), heads = (1, 2, 5, 8), ff_expansion = (8, 8, 4, 4),
+        reduction_ratio = (8, 4, 2, 1), num_layers = 2, channels = 3, decoder_dim = 256, num_classes = 2
+    ):
+        super().__init__()
+        dims, heads, ff_expansion, reduction_ratio, num_layers = map(partial(cast_tuple, depth = 4), (dims, heads, ff_expansion, reduction_ratio, num_layers))
+        assert all([*map(lambda t: len(t) == 4, (dims, heads, ff_expansion, reduction_ratio, num_layers))]), 'only four stages are allowed, all keyword arguments must be either a single value or a tuple of 4 values'
+
+        self.mit = MiT(
+            channels = channels,
+            dims = dims,
+            heads = heads,
+            ff_expansion = ff_expansion,
+            reduction_ratio = reduction_ratio,
+            num_layers = num_layers
+        )
+        
+        ## 改进类似deeplabv3plus解码结构（增加低维卷积特征提取）             &&-----已训练-----&&
+        self.low_conv = SingleConv(3, dims[1], kernel_size=(7, 7), stride=(2, 2), padding=(3, 3))
+
+        self.to_fused = nn.ModuleList([nn.Sequential(
+            nn.Conv2d(dim, decoder_dim, 1),
+            nn.Upsample(scale_factor = 2 ** i)
+        ) for i, dim in enumerate(dims)])
+
+        self.conv1 = SingleConv(dims[-1]*4,dims[-1],1)
+
+        self.to_segmentation = nn.Sequential(
+            SingleConv((decoder_dim+dims[1]), decoder_dim, 1),
+            nn.Conv2d(decoder_dim, num_classes, 1),
+            ##(2,2,128,128)
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
+        )
+
+    def forward(self, x):
+
+        # 增加低维卷积特征提取
+        x_l= self.low_conv(x)
+        # (1,64,256,256)
+
+        layer_outputs = self.mit(x, return_layer_outputs = True)
+        #(2,32,128,128),(2,64,64,64),(2,160,32,32),(2,256,16,16)
+
+        fused = [to_fused(output) for output, to_fused in zip(layer_outputs, self.to_fused)]
+        fused = torch.cat(fused, dim = 1)
+        # (2,1024,128,128)
+
+        fused = F.interpolate(self.conv1(fused),scale_factor=2,mode="bilinear")
+        # (2,256,256,256)
+
+        fused = torch.cat([fused,x_l], dim = 1)
+        # (2,320,256,256)
+        output = self.to_segmentation(fused)
+        return output
    
 def init_weights(m):
     classname = m.__class__.__name__
